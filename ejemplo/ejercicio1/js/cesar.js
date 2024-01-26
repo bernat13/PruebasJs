c69aa766d5aaae5e4091f07fea7b814d46ebea47
Bernat <bernat13@gmail.com>
2024-01-26 09:14:02 +0100

    casi resuelto

--- ejemplo/ejercicio1/js/cesar.js
+++ ejemplo/ejercicio1/js/cesar.js
@@ -1,13 +1,14 @@
 console.log("cesar.js cargado")
-var desplazamiento = 1
 
-function cifrarCesar(texto, desplazamiento) {
+
+function cifrarCesar(texto) {
+    var desplazamiento = 10;
     texto = texto.toUpperCase();
     var textoCifrado = "";
     var posicion = 0;
     var posicionCifrada = 0;
     var letraCifrada = "";
-    var diccionario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.";
+    var diccionario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.0123456789ÑÇ";
     for (var i = 0; i < texto.length; i++) {
         letra = texto[i];
         posicion = diccionario.indexOf(letra)
@@ -16,18 +17,19 @@ function cifrarCesar(texto, desplazamiento) {
             posicionCifrada = 0;
         }
         letraCifrada = diccionario[posicionCifrada];
-        textoCifrado += letraCifrada;
+        textoCifrado =textoCifrado +  letraCifrada;
     }
     return textoCifrado;
 }
 
-function descifrarCesar(texto, desplazamiento) {
+function descifrarCesar(texto) {
+    var desplazamiento = 10;
     texto = texto.toUpperCase();
     var textoDescifrado = "";
     var posicion = 0;
     var posicionDescifrada = 0;
     var letraDescifrada = "";
-    var diccionario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.";
+    var diccionario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.0123456789ÑÇ";
 
     for (var i = 0; i < texto.length; i++) {
         letra = texto[i];
@@ -42,5 +44,49 @@ function descifrarCesar(texto, desplazamiento) {
     return textoDescifrado;
 }
 
-console.log(cifrarCesar("HOLA.", desplazamiento))
-console.log(descifrarCesar("IPMBa", desplazamiento))
+console.log(cifrarCesar("HOLA."))
+console.log(descifrarCesar("IPMBa"))
+
+
+
+
+
+
+function apretarbotonencripta(){
+    //recuperar lo que hay en el input
+    var elemtexto = document.querySelector("#textenc");
+    var texto = elemtexto.value;
+//calcular lo que sea
+    var res = cifrarCesar(texto);   
+
+    //pintar resultado
+    var parraf = document.querySelector("#resultadoenc");
+    
+    parraf.innerHTML=res;
+
+
+}
+
+var boton = document.querySelector("#encripta");
+boton.addEventListener("mouseover",apretarbotonencripta);
+
+
+
+
+function apretarbotondesencripta(){
+    //recuperar lo que hay en el input
+    var elemtexto = document.querySelector("#textdes");
+    var texto = elemtexto.value;
+//calcular lo que sea
+    var res = descifrarCesar(texto);   
+
+    //pintar resultado
+    var parraf = document.querySelector("#resultadodes  ");
+    
+    parraf.innerHTML=res;
+
+
+}
+
+var botondesc = document.querySelector("#desencripta");
+botondesc.addEventListener("click",apretarbotondesencripta);
--- ejemplo/ejercicio1/js/factorial.js
+++ ejemplo/ejercicio1/js/factorial.js
@@ -9,4 +9,20 @@ function factorial(n) {
 }
 
 
-console.log(factorial(5))
+function apretarboton(){
+    //recuperar lo que hay en el input
+    var elemnumero = document.querySelector("#numero");
+    var num = elemnumero.value;
+//calcular lo que sea
+    var res = factorial(num);   
+
+    //pintar resultado
+    var parraf = document.querySelector("#resultado");
+    
+    parraf.innerHTML="<h2>El resultado es " + res+ "</h2>";
+
+
+}
+
+var boton = document.querySelector("#boton");
+boton.addEventListener("click",apretarboton);
